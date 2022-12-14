a47564bea8fca02a467f51df657df86e1da02cf0
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-14 21:56:10 +0600

    add more purchase

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -145,6 +145,8 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
     <script src="{{ asset('backend/assets/js/code.js') }}"></script>
+    <script src="{{ asset('backend/assets/js/handlebars.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
 </body>
 
 </html>
--- resources/views/purchase/create.blade.php
+++ resources/views/purchase/create.blade.php
@@ -10,7 +10,7 @@
                 <div class="card">
                     <div class="card-body">
 
-                        <h4 class="card-title">Add Product </h4><br><br>
+                        <h4 class="card-title">Add Product</h4><br><br>
 
                         <div class="row">
                             <div class="col-md-4">
@@ -60,7 +60,7 @@
                                     <label for="example-text-input" class="form-label">Product Name </label>
                                     <select name="product_id" id="product_id" class="form-select"
                                         aria-label="Default select example">
-                                        <option selected="">Select Category</option>
+                                        <option selected="">Select Product</option>
 
                                     </select>
                                 </div>
@@ -71,14 +71,57 @@
                                 <div class="md-3">
                                     <label for="example-text-input" class="form-label" style="margin-top:43px;">
                                     </label>
-                                    <input type="submit" class="btn btn-secondary btn-rounded waves-effect waves-light"
-                                        value="Add More">
+
+                                    <i
+                                        class="btn btn-secondary btn-rounded waves-effect waves-light fas fa-plus-circle addeventmore">
+                                        Add More</i>
                                 </div>
                             </div>
 
                         </div> <!-- // end row  -->
 
                     </div> <!-- End card-body -->
+
+                    <div class="card-body">
+                        <form action="" method="">
+                            @csrf
+                            <table class="table-sm table-bordered" width="100%" style="border-color: #ddd;">
+                                <thead>
+                                    <tr>
+                                        <th>Category</th>
+                                        <th>Product</th>
+                                        <th>Unit</th>
+                                        <th>Unit Price</th>
+                                        <th>Description</th>
+                                        <th>Total Price</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="addRow" class="addRow">
+
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                        <td colspan="5"></td>
+                                        <td>
+                                            <input type="text" name="estimated_amount" value="0" id="estimated_amount"
+                                                class="form-control estimated_amount" readonly
+                                                style="background-color: #ddd">
+                                        </td>
+                                        <td>
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <br>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-info" id="storeButton">
+                                    Save Purchase
+                                </button>
+                            </div>
+                        </form>
+                    </div>
 
                 </div>
 
@@ -87,6 +130,103 @@
         </div>
     </div>
 </div>
+
+
+
+<script id="document-template" type="text/x-handlebars-template">
+
+    <tr class="delete_add_more_item" id="delete_add_more_item">
+            <input type="hidden" name="date[]" value="@{{date}}">
+            <input type="hidden" name="purchase_no[]" value="@{{purchase_no}}">
+            <input type="hidden" name="supplier_id[]" value="@{{supplier_id}}">
+
+        <td>
+            <input type="hidden" name="category_id[]" value="@{{category_id}}">
+            @{{ category_name }}
+        </td>
+
+         <td>
+            <input type="hidden" name="product_id[]" value="@{{product_id}}">
+            @{{ product_name }}
+        </td>
+
+         <td>
+            <input type="number" min="1" class="form-control buying_qty text-right" name="buying_qty[]" value="">
+        </td>
+
+        <td>
+            <input type="number" class="form-control unit_price text-right" name="unit_price[]" value="">
+        </td>
+
+     <td>
+            <input type="text" class="form-control" name="description[]">
+        </td>
+
+         <td>
+            <input type="number" class="form-control buying_price text-right" name="buying_price[]" value="0" readonly>
+        </td>
+
+         <td>
+            <i class="btn btn-danger btn-sm fas fa-window-close removeeventmore"></i>
+        </td>
+
+        </tr>
+
+    </script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(document).on("click",".addeventmore", function(){
+            var date = $('#date').val();
+            var purchase_no = $('#purchase_no').val();
+            var supplier_id = $('#supplier_id').val();
+            var category_id  = $('#category_id').val();
+            var category_name = $('#category_id').find('option:selected').text();
+            var product_id = $('#product_id').val();
+            var product_name = $('#product_id').find('option:selected').text();
+
+
+            if(date == ''){
+                $.notify("Date is Required" ,  {globalPosition: 'top right', className:'error' });
+                return false;
+                 }
+                  if(purchase_no == ''){
+                $.notify("Purchase No is Required" ,  {globalPosition: 'top right', className:'error' });
+                return false;
+                 }
+
+                  if(supplier_id == ''){
+                $.notify("Supplier is Required" ,  {globalPosition: 'top right', className:'error' });
+                return false;
+                 }
+                  if(category_id == ''){
+                $.notify("Category is Required" ,  {globalPosition: 'top right', className:'error' });
+                return false;
+                 }
+                  if(product_id == ''){
+                $.notify("Product Field is Required" ,  {globalPosition: 'top right', className:'error' });
+                return false;
+                 }
+
+
+                 var source = $("#document-template").html();
+                 var template = Handlebars.compile(source);
+                 var data = {
+                    date:date,
+                    purchase_no:purchase_no,
+                    supplier_id:supplier_id,
+                    category_id:category_id,
+                    category_name:category_name,
+                    product_id:product_id,
+                    product_name:product_name
+
+                 };
+                 var html = template(data);
+                 $("#addRow").append(html);
+        });
+    })
+
+</script>
 
 <script type="text/javascript">
     $(function(){
@@ -117,7 +257,7 @@
                 type: "GET",
                 data:{category_id:category_id},
                 success:function(data){
-                    var html = '<option value="">Select Category</option>';
+                    var html = '<option value="">Select Product</option>';
                     $.each(data,function(key,v){
                         html += '<option value=" '+v.id+' "> '+v.name+'</option>';
                     });
